820fd6697049c127413723e1dbe8200866770858
Simone Sanna <38055953+Paulpaccy@users.noreply.github.com>
2023-12-20 16:18:02 +0100

    Caricamente ModApp

---
+++ ProgettoTecWeb/src/ModApp/Assets/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// main.js
+
+// Aggiungi il tuo codice JavaScript qui
+
+// Esempio di selezione di un elemento HTML e manipolazione
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleziona un elemento con un certo ID
+  console.log("ciaoooo");
+  let myElement = document.getElementById("myElementId");
+
+  // Manipola l'elemento (esempio: cambia il testo)
+  if (myElement) {
+    myElement.textContent = "Nuovo testo";
+  }
+
+  // Aggiungi altri manipolazioni o eventi a seconda delle tue esigenze
+});
+
+// Puoi continuare ad aggiungere il tuo codice JavaScript qui a seconda delle necessità del tuo sito
