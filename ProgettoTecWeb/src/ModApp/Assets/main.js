8b4ff9165d5db402f134da73bc908366394da247
Simone Sanna <38055953+Paulpaccy@users.noreply.github.com>
2023-12-20 23:42:42 +0100

    Test ModDashboard

--- ProgettoTecWeb/src/ModApp/Assets/main.js
+++ ProgettoTecWeb/src/ModApp/Assets/main.js
@@ -1,21 +1,133 @@
 "use strict";
+//const user = await getModerator();
+//const squeals = await getReports();
+const usernameInput = Document.getElementById('userInput');
+const channelInput = document.getElementById('channelInput');
+const squealInput = document.getElementById('squealInput');
 
-// main.js
 
-// Aggiungi il tuo codice JavaScript qui
 
-// Esempio di selezione di un elemento HTML e manipolazione
 document.addEventListener("DOMContentLoaded", function () {
-  // Seleziona un elemento con un certo ID
-  console.log("ciaoooo");
-  let myElement = document.getElementById("myElementId");
-
-  // Manipola l'elemento (esempio: cambia il testo)
-  if (myElement) {
-    myElement.textContent = "Nuovo testo";
-  }
-
-  // Aggiungi altri manipolazioni o eventi a seconda delle tue esigenze
 });
 
-// Puoi continuare ad aggiungere il tuo codice JavaScript qui a seconda delle necessità del tuo sito
+document.getElementById("userInput").addEventListener("submit", function (e) { 
+  e.preventDefault(); 
+ 
+  let formData = new FormData(form); 
+  console.log(formData);
+  // output as an object 
+  console.log(Object.fromEntries(formData)); 
+ 
+  // ...or iterate through the name-value pairs 
+  for (let pair of formData.entries()) { 
+    console.log(pair[0] + ": " + pair[1]); 
+  } 
+});
+
+// Define the API URL
+async function getModerator() {
+  const identifier = window.localStorage.getItem('user') || window.sessionStorage.getItem('user');
+  if(!identifier) {
+    ;
+  }
+  const apiUrl = 'user/' + identifier;
+
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+async function getReports() {
+  const apiUrl = 'squeals';
+  const outputElement = document.getElementById('reports');
+
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      outputElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+async function getUser() {
+  const apiUrl = 'user/' + usernameInput.value;
+  const outputElement = document.getElementById('user');
+
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      outputElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+async function getChannel() {
+  const apiUrl = 'channels/' + channelInput.value;
+  const outputElement = document.getElementById('channel');
+
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      outputElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+async function getSqueal() {
+  const apiUrl = 'squeals/' + squealInput.value;
+  const outputElement = document.getElementById('squeal');
+
+  // Make a GET request
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      outputElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
