357d406db8020f07ca622bd73af0c27af6d555b7
Ilaria Palestini <75267985+Ilariapale@users.noreply.github.com>
2024-02-07 17:52:32 +0100

    Update Characters.vue

--- ProgettoTecWeb/src/SmmApp/src/components/gadgets/Characters.vue
+++ ProgettoTecWeb/src/SmmApp/src/components/gadgets/Characters.vue
@@ -52,21 +52,21 @@
                                     <td class="p-2 px-0" style="white-space: pre;"><b>Daily</b>{{ '\t' }}<i
                                             class="bi bi-arrow-right"></i> <button type="button"
                                             :disabled="vip.username == undefined" class="btn btn-primary p-1 px-2"
-                                            @click="buyChars('daily')">Buy 50</button>
+                                            @click="buyChars('daily')">Buy 500</button>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td class="p-2 px-0" style="white-space: pre;"><b>Weekly</b>{{ '\t' }}<i
                                             class="bi bi-arrow-right"></i> <button type="button"
                                             :disabled="vip.username == undefined" class="btn btn-primary p-1 px-2"
-                                            @click="buyChars('weekly')">Buy 150</button>
+                                            @click="buyChars('weekly')">Buy 1500</button>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td class="p-2 px-0" style="white-space: pre;"><b>Monthly</b>{{ '\t' }}<i
                                             class="bi bi-arrow-right"></i> <button type="button"
                                             :disabled="vip.username == undefined" class="btn btn-primary p-1 px-2"
-                                            @click="buyChars('monthly')">Buy 500</button>
+                                            @click="buyChars('monthly')">Buy 5000</button>
                                     </td>
                                 </tr>
                             </tbody>
